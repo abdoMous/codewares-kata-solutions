14350565bd89ae8277793ba8a12cadc213602e6d
Abderrahman MOUSSAOUI <abdrrahmane.moussaoui@gmail.com>
2017-11-21 13:51:48 +0100

    add 7kyu solution: Disemoveowel Trolls

---
+++ 7kyu/disemvowel-trolls.js
@@ -0,0 +1,21 @@
+// https://www.codewars.com/kata/disemvowel-trolls
+
+// Trolls are attacking your comment section!
+//
+//     A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+//
+// Your task is to write a function that takes a string and return a new string with all vowels removed.
+//
+//     For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+//
+//     Note: for this kata y isn't considered a vowel.
+
+const disemvowel = str =>
+    str.split("")
+        .filter(c =>
+        c.toLowerCase() !== "a" &&
+        c.toLowerCase() !== "u" &&
+        c.toLowerCase() !== "o" &&
+        c.toLowerCase() !== "e" &&
+        c.toLowerCase() !== "i").join("")
+
